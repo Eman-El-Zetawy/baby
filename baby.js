348c80a01f44194710605bfc8f46edabfb7cbf5e
Eman-El-Zetawy <dovprogrammer@gmail.com>
2019-10-21 09:09:42 -0700

    new10 21

--- baby.js
+++ baby.js
@@ -1,9 +1,7 @@
     var  img = document.getElementsByTagName("img");
     var photo = document.getElementById("photo");
     var src , value , index =[0 , 1 , 2 , 3, 4, 5] , newSrc , text =["image-1" , "image-2" , "image-3" , "image-4" ,"image-5" , "image-6" ];
-index.forEach(r=>{
-    img[r].addEventListener("click" , click_image);
-    });
+index.forEach(r=>{   img[r].addEventListener("click" , click_image); });
 function click_image (event){
     value = event.target.getAttribute("id");
     src =event.target.getAttribute("src");
@@ -36,11 +34,12 @@ function move(event){
      return ;  }  }
 function draw (src){
     photo.innerHTML ='<img  src=' + src + ' class="new_img" / > ';
-    photo.innerHTML +='<span  class="box_x" onclick="click_x()">X</span>' ; 
+    photo.innerHTML +='<a href="#" class="box_x" onclick="click_x()">X</a>' ; 
     photo.style ="position: absolute ";
     photo.innerHTML +=' <p class="para">'+text[value]+'</p>';
-    photo.innerHTML +='<div id="left" class="left" onclick="move(event)">'+'<span class="back">back</span>'+'</div><div id="right" class="right" onclick="move(event)">'+'<span class="next">next</span>'+'</div>';
-if(value==5){   document.getElementById("right").style.color ="gainsboro";}
-if(value==0){   document.getElementById("left").style.color ="gainsboro";}
+    photo.innerHTML +='<a href="#" id="left" class="left" onclick="move(event)">'+'<a href="#" id="left" onclick="move(event)" class="back">back</a>'+'</a><a  href="#" id="right" class="right" onclick="move(event)">'+'<a href="#"  id="right" class="next" onclick="move(event)" >next</a>'+'</a>';
+if(value==5){   document.getElementsByClassName("next")[0].style.color ="gainsboro";}                                                                                                                                                                         
+if(value==0){   document.getElementsByClassName("back")[0].style.color ="gainsboro";}
 
               }
+            //  
